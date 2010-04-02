30f40222e7a0e3ce0ed1c2430af91157fa8ab5c7
cloudhead <self@cloudhead.net>
2010-04-01 22:37:29 -0400

    Array.isArray compat

--- build/compat.js
+++ build/compat.js
@@ -3,3 +3,12 @@ if (typeof(String.prototype.trim) !== "function") {
         return this.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
     };
 }
+if (typeof(Array.isArray) !== "function") {
+    Array.isArray = function (a) {
+        if (a instanceof Array) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}

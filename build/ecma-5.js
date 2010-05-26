ff0dbb0a146688abda5bf6233eb17319d223b753
cloudhead <self@cloudhead.net>
2010-05-26 13:37:53 -0400

    cleanup ecma-5.js

--- build/ecma-5.js
+++ build/ecma-5.js
@@ -1,27 +1,19 @@
 
+// ecma-5.js
+//
 // -- kriskowal Kris Kowal Copyright (C) 2009-2010 MIT License
 // -- tlrobinson Tom Robinson
 // dantman Daniel Friesen
 
-/*!
-    Copyright (c) 2009, 280 North Inc. http://280north.com/
-    MIT License. http://github.com/280north/narwhal/blob/master/README.md
-*/
-
-
 //
 // Array
-// =====
 //
-
-// ES5 15.4.3.2
 if (!Array.isArray) {
     Array.isArray = function(obj) {
-        return Object.prototype.toString.call(obj) == "[object Array]";
+        return Object.prototype.toString.call(obj) === "[object Array]" ||
+               (obj instanceof Array);
     };
 }
-
-// ES5 15.4.4.18
 if (!Array.prototype.forEach) {
     Array.prototype.forEach =  function(block, thisObject) {
         var len = this.length >>> 0;
@@ -32,51 +24,40 @@ if (!Array.prototype.forEach) {
         }
     };
 }
-
-// ES5 15.4.4.19
-// https://developer.mozilla.org/en/Core_JavaScript_1.5_Reference/Objects/Array/map
 if (!Array.prototype.map) {
     Array.prototype.map = function(fun /*, thisp*/) {
         var len = this.length >>> 0;
-        if (typeof fun != "function")
-          throw new TypeError();
-
         var res = new Array(len);
         var thisp = arguments[1];
-        for (var i = 0; i < len; i++) {
-            if (i in this)
-                res[i] = fun.call(thisp, this[i], i, this);
-        }
 
+        for (var i = 0; i < len; i++) {
+            if (i in this) {
+                res[i] = fun.call(thisp, this[i], i, this);
+            }
+        }
         return res;
     };
 }
-
-// ES5 15.4.4.20
 if (!Array.prototype.filter) {
     Array.prototype.filter = function (block /*, thisp */) {
         var values = [];
         var thisp = arguments[1];
-        for (var i = 0; i < this.length; i++)
-            if (block.call(thisp, this[i]))
+        for (var i = 0; i < this.length; i++) {
+            if (block.call(thisp, this[i])) {
                 values.push(this[i]);
+            }
+        }
         return values;
     };
 }
-
-// ES5 15.4.4.21
-// https://developer.mozilla.org/en/Core_JavaScript_1.5_Reference/Objects/Array/reduce
 if (!Array.prototype.reduce) {
     Array.prototype.reduce = function(fun /*, initial*/) {
         var len = this.length >>> 0;
-        if (typeof fun != "function")
-            throw new TypeError();
+        var i = 0;
 
         // no value to return if no initial value and an empty array
-        if (len == 0 && arguments.length == 1)
-            throw new TypeError();
+        if (len === 0 && arguments.length === 1) throw new TypeError();
 
-        var i = 0;
         if (arguments.length >= 2) {
             var rv = arguments[1];
         } else {
@@ -85,39 +66,30 @@ if (!Array.prototype.reduce) {
                     rv = this[i++];
                     break;
                 }
-
                 // if array contains no values, no initial value to return
-                if (++i >= len)
-                    throw new TypeError();
+                if (++i >= len) throw new TypeError();
             } while (true);
         }
-
         for (; i < len; i++) {
-            if (i in this)
+            if (i in this) {
                 rv = fun.call(null, rv, this[i], i, this);
+            }
         }
-
         return rv;
     };
 }
-
-
-// ES5 15.4.4.14
 if (!Array.prototype.indexOf) {
     Array.prototype.indexOf = function (value /*, fromIndex */ ) {
         var length = this.length;
-        if (!length)
-            return -1;
         var i = arguments[1] || 0;
-        if (i >= length)
-            return -1;
-        if (i < 0)
-            i += length;
+
+        if (!length)     return -1;
+        if (i >= length) return -1;
+        if (i < 0)       i += length;
+
         for (; i < length; i++) {
-            if (!Object.prototype.hasOwnProperty.call(this, i))
-                continue;
-            if (value === this[i])
-                return i;
+            if (!Object.prototype.hasOwnProperty.call(this, i)) { continue }
+            if (value === this[i]) return i;
         }
         return -1;
     };
@@ -125,10 +97,7 @@ if (!Array.prototype.indexOf) {
 
 //
 // Object
-// ======
 //
-
-// ES5 15.2.3.14
 if (!Object.keys) {
     Object.keys = function (object) {
         var keys = [];
@@ -143,15 +112,9 @@ if (!Object.keys) {
 
 //
 // String
-// ======
 //
-
-// ES5 15.5.4.20
 if (!String.prototype.trim) {
-    // http://blog.stevenlevithan.com/archives/faster-trim-javascript
-    var trimBeginRegexp = /^\s\s*/;
-    var trimEndRegexp = /\s\s*$/;
     String.prototype.trim = function () {
-        return String(this).replace(trimBeginRegexp, '').replace(trimEndRegexp, '');
+        return String(this).replace(/^\s\s*/, '').replace(/\s\s*$/, '');
     };
 }

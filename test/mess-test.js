c72aed5b1138867bf49d7406432253be43c0bbeb
Konstantin Käfer <github@kkaefer.com>
2011-01-20 14:41:27 -0500

    update test framework to use expresso

--- test/mess-test.js
+++ test/mess-test.js
@@ -3,11 +3,6 @@ var path = require('path'),
     sys = require('sys'),
     assert = require('assert');
 
-require.paths.unshift(
-    path.join(__dirname, '../lib/node'),
-    path.join(__dirname, '../lib')
-);
-
 var mess = require('mess');
 var tree = require('mess/tree');
 var xml2js = require('xml2js');
@@ -16,78 +11,93 @@ var xml2js = require('xml2js');
 var messDir = path.join(__dirname, 'mess');
 var resultDir = path.join(__dirname, 'xml');
 
-fs.readdirSync(messDir).forEach(function (file) {
-    if (! /\.mss$/.test(file)) return;
+exports['test compilation'] = function(beforeExit) {
+    var failures = 0;
+    fs.readdirSync(messDir).forEach(function (file) {
+        if (! /\.mss$/.test(file)) return;
 
-    toCSS(path.join(messDir, file), function (err_compile, mess_result) {
-        var name = path.basename(file, '.mss');
+        toCSS(path.join(messDir, file), function (err_compile, mess_result) {
+            var name = path.basename(file, '.mss');
 
-        if (err_compile) {
-            // Compilation failed. This may be intentional. In this case, we
-            // have a JSON file that contains the excpected error description.
-            fs.readFile(path.join(resultDir, name + '.json'), 'utf-8', function (err_result, json_result) {
-                if (err_result) {
-                    // This is a test failure; the test should compile properly
-                    // but does not.
-                    console.log(stylize("FAIL", 'red') + ': compilation failed for ' + stylize(file, 'underline') + '.');
-                    showErrors(err_compile);
-                } else {
-                    // For this test, compilation should fail. We now check whether
-                    // we got the expected errors.
-                    try {
-                        json_result = JSON.parse(json_result);
-                    } catch (e) {
-                        console.log(stylize("FAIL", 'red') + ': JSON result file ' + stylize(name + '.json', 'underline') + ' is invalid.');
-                        return;
+            if (err_compile) {
+                // Compilation failed. This may be intentional. In this case, we
+                // have a JSON file that contains the excpected error description.
+                fs.readFile(path.join(resultDir, name + '.json'), 'utf-8', function (err_result, json_result) {
+                    if (err_result) {
+                        // This is a test failure; the test should compile properly
+                        // but does not.
+                        console.log(stylize("FAIL", 'red') + ': compilation failed for ' + stylize(file, 'underline') + '.');
+                        failures++;
+                        showErrors(err_compile);
+                    } else {
+                        // For this test, compilation should fail. We now check whether
+                        // we got the expected errors.
+                        try {
+                            json_result = JSON.parse(json_result);
+                        } catch (e) {
+                            console.log(stylize("FAIL", 'red') + ': JSON result file ' + stylize(name + '.json', 'underline') + ' is invalid.');
+                            failures++;
+                            return;
+                        }
+
+                        try {
+                            assert.deepEqual(json_result, err_compile);
+                        } catch (e) {
+                            console.log(stylize("FAIL", 'red') + ': Errors in ' + stylize(name + '.json', 'underline') + ' differ from actual errors.');
+                            console.log('    ' + stylize('actual', 'bold') + ' ' + sys.inspect(e.actual).replace(/\n/g, '\n           '));
+                            console.log('  ' + stylize('expected', 'bold') + ' ' + sys.inspect(e.expected).replace(/\n/g, '\n           '));
+                            console.log('');
+                            failures++;
+                            return;
+                        }
+
+                        console.log(stylize("SUCCESS", 'green') + ': ' + stylize(file, 'underline') + ' passed test.');
                     }
+                });
+            }
+            else {
+                // Compilation succeeded
+                fs.readFile(path.join(resultDir, name + '.xml'), 'utf-8', function (err_result, xml_result) {
+                    if (err_result) {
+                        // The xml file is not present. This means that compilation
+                        // should not have succeeded.
+                        console.log(stylize("FAIL", 'red') + ': compilation succeeded for ' + stylize(file, 'underline') + ', but there is no result XML.');
+                        failures++;
+                    } else {
+                        // Parse the XML file.
+                        var resultParser = new xml2js.Parser();
+                        resultParser.addListener('end', function(resultXML) {
+                            var messParser = new xml2js.Parser();
+                            messParser.addListener('end', function(messXML) {
+                                try {
+                                    assert.deepEqual(resultXML, messXML);
+                                } catch (e) {
+                                    assert.ok(false, 'XML output for ' + stylize(file, 'underline') + ' differs expected XML.');
+                                    // console.log(stylize("FAIL", 'red') + ': ');
+                                    console.log('    ' + stylize('actual', 'bold') + ' ' + sys.inspect(e.actual, false, null).replace(/\n/g, '\n           '));
+                                    console.log('  ' + stylize('expected', 'bold') + ' ' + sys.inspect(e.expected, false, null).replace(/\n/g, '\n           '));
+                                    console.log('');
+                                    failures++;
+                                    return;
+                                }
 
-                    try {
-                        assert.deepEqual(json_result, err_compile);
-                    } catch (e) {
-                        console.log(stylize("FAIL", 'red') + ': Errors in ' + stylize(name + '.json', 'underline') + ' differ from actual errors.');
-                        console.log('    ' + stylize('actual', 'bold') + ' ' + sys.inspect(e.actual).replace(/\n/g, '\n           '));
-                        console.log('  ' + stylize('expected', 'bold') + ' ' + sys.inspect(e.expected).replace(/\n/g, '\n           '));
-                        console.log('');
-                        return;
-                    }
-
-                    console.log(stylize("SUCCESS", 'green') + ': ' + stylize(file, 'underline') + ' passed test.');
-                }
-            });
-        }
-        else {
-            // Compilation succeeded
-            fs.readFile(path.join(resultDir, name + '.xml'), 'utf-8', function (err_result, xml_result) {
-                if (err_result) {
-                    // The xml file is not present. This means that compilation
-                    // should not have succeeded.
-                    console.log(stylize("FAIL", 'red') + ': compilation succeeded for ' + stylize(file, 'underline') + ', but there is no result XML.');
-                } else {
-                    // Parse the XML file.
-                    var resultParser = new xml2js.Parser();
-                    resultParser.addListener('end', function(resultXML) {
-                        var messParser = new xml2js.Parser();
-                        messParser.addListener('end', function(messXML) {
-                            try {
-                                assert.deepEqual(resultXML, messXML);
-                            } catch (e) {
-                                console.log(stylize("FAIL", 'red') + ': XML output for ' + stylize(file, 'underline') + ' differs expected XML.');
-                                console.log('    ' + stylize('actual', 'bold') + ' ' + sys.inspect(e.actual, false, null).replace(/\n/g, '\n           '));
-                                console.log('  ' + stylize('expected', 'bold') + ' ' + sys.inspect(e.expected, false, null).replace(/\n/g, '\n           '));
-                                console.log('');
-                                return;
-                            }
-
-                            console.log(stylize("SUCCESS", 'green') + ': ' + stylize(file, 'underline') + ' passed test.');
+                                console.log(stylize("SUCCESS", 'green') + ': ' + stylize(file, 'underline') + ' passed test.');
+                            });
+                            messParser.parseString('<Map>' + mess_result + '</Map>');
                         });
-                        messParser.parseString('<Map>' + mess_result + '</Map>');
-                    });
-                    resultParser.parseString('<Map>' + xml_result + '</Map>');
-                }
-            });
+                        resultParser.parseString('<Map>' + xml_result + '</Map>');
+                    }
+                });
+            }
+        });
+    });
+    
+    beforeExit(function() {
+        if (failures) {
+            assert.ok(false, failures + ' compilations failed.');
         }
     });
-});
+};
 
 function toCSS(filename, callback) {
     var tree, css;

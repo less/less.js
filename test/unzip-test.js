3d6443c6c518c3576cfec8888ec55fc959fd3808
Dane Springmeyer <dane@dbsgeo.com>
2011-01-20 18:56:54 -0500

    support for node-unzip, new espresso test, and support for reading local zipped shapefiles

---
+++ test/unzip-test.js
@@ -0,0 +1,20 @@
+var path = require('path')
+var assert = require('assert')
+var mess = require('mess');
+var external = require('mess/external')
+
+exports['test_unzip_remote'] = function(){
+    var e = external();
+    //var remote = 'http://cascadenik-sampledata.s3.amazonaws.com/world_borders.zip';
+    var remote = '/Users/dane/projects/utils/data/world_borders.zip';
+    e.process(remote,function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        //assert.equal(result[0],remote);
+        //assert.ok(result[1]);
+        //assert.equal(path.dirname(result[1]),e.pos(remote));
+      }
+    )
+};
+
+exports['test_unzip_remote']()

e18ae539fc0cc941531b47025443829294ead1d1
Tom MacWright <macwright@gmail.com>
2011-01-24 10:23:58 -0500

    Cleanining up unzip test

--- test/unzip-test.js
+++ test/unzip-test.js
@@ -1,16 +1,18 @@
-var path = require('path')
-var assert = require('assert')
-var mess = require('mess');
-var external = require('mess/external')
+var path = require('path'),
+    assert = require('assert'),
+    mess = require('mess'),
+    External = require('mess/external');
 
-exports['test_unzip_remote'] = function(){
-    var e = external();
+exports['test_unzip_remote'] = function() {
+    var e = new External({
+        data_dir: '.'
+    });
     var remote = 'http://cascadenik-sampledata.s3.amazonaws.com/world_borders.zip';
-    e.process(remote,function(err, result) {
-        if (err) throw err;
-        assert.equal(result[0],remote);
+    e.process(remote, function(err, result) {
+        assert.eql(err, null);
+        assert.eql(result[0], remote);
         assert.ok(result[1]);
-        assert.equal(path.dirname(result[1]),e.pos(remote));
+        assert.eql(path.dirname(result[1]), e.pos(remote));
       }
-    )
+    );
 };

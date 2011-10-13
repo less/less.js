d607326cc702a8bb379afec1d34713ccb6855799
Tom MacWright <tom@macwright.org>
2011-10-13 11:12:50 -0400

    Don't tolerate no-selector elements, tolerate Map, adds tests

--- test/errorhandling.test.js
+++ test/errorhandling.test.js
@@ -12,12 +12,22 @@ helper.files('errorhandling', 'mml', function(file) {
         var completed = false;
         var renderResult;
         var mml = helper.mml(file);
-        new carto.Renderer({
-            paths: [ path.dirname(file) ],
-            data_dir: path.join(__dirname, '../data'),
-            local_data_dir: path.join(__dirname, 'rendering'),
-            filename: file
-        }).render(mml, function (err) {
+        try {
+            new carto.Renderer({
+                paths: [ path.dirname(file) ],
+                data_dir: path.join(__dirname, '../data'),
+                local_data_dir: path.join(__dirname, 'rendering'),
+                filename: file
+            }).render(mml, function (err) {
+                var result = helper.resultFile(file);
+                var output = err.message;
+                // @TODO for some reason, fs.readFile includes an additional \n
+                // at the end of read files. Determine why.
+                fs.readFile(helper.resultFile(file), 'utf8', function(err, data) {
+                    if (!err) assert.deepEqual(output, data.substr(0, data.length - 1));
+                });
+            });
+        } catch(err) {
             var result = helper.resultFile(file);
             var output = err.message;
             // @TODO for some reason, fs.readFile includes an additional \n
@@ -25,7 +35,8 @@ helper.files('errorhandling', 'mml', function(file) {
             fs.readFile(helper.resultFile(file), 'utf8', function(err, data) {
                 if (!err) assert.deepEqual(output, data.substr(0, data.length - 1));
             });
-        });
+        }
+
 
         beforeExit(function() {
             /*

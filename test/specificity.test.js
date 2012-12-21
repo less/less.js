75f55f8d04d320ec55baa2c8cf305e00490c5942
Tom MacWright <tom@macwright.org>
2012-12-21 18:13:36 -0500

    Update specificity test helper

--- test/specificity.test.js
+++ test/specificity.test.js
@@ -12,7 +12,7 @@ function cleanupItem(key, value) {
     else if (key === 'elements') return value.map(function(item) { return item.value; });
     else if (key === 'filters') {
         var arr = [];
-        for (var id in value) arr.push(id + value[id].val);
+        for (var id in value.filters) arr.push(id + value.filters[id].val);
         if (arr.length) return arr;
     }
     else if (key === 'attachment' && value === '__default__') return;

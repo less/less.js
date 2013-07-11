3d87cd490d3f06ba25672a2941fa2b9082b4a1ce
javi <qualopec@gmail.com>
2013-07-11 16:14:32 +0200

    fixed global imports

--- browser/require.js
+++ browser/require.js
@@ -1,10 +1,20 @@
 //
 // Stub out `require` in the browser
 //
+window.carto = window.carto || {};
+window.carto.underscore = window._;
+
 function require(arg) {
     var mod = window.carto[arg];
     if(!mod) {
       mod = window.carto[arg.split('/')[1]];
+    }
+    if(!mod) {
+      mod = window.carto[arg]
+    }
+    // try global scope
+    if(!mod) {
+      mod = window[arg]
     }
     return mod;
 }

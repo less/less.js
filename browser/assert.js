f3fd24583d1ed62707bd2a2cc53dc720dec52681
javi <qualopec@gmail.com>
2013-11-05 08:52:08 +0100

    fixed IE7 and IE8 script loading

--- browser/assert.js
+++ browser/assert.js
@@ -4,4 +4,10 @@ var assert = {
     message = message || '';
     if(!value) throw new Error("assertion failed", message);
   }
+};
+
+if (navigator.userAgent.indexOf('MSIE 8.0') !== -1 || navigator.userAgent.indexOf('MSIE 7.0') !== -1) {
+  Object.defineProperty = function(o, p, fn) { o[p] = fn.value; };
 }
+
+

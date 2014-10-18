8562e2cad89d7ca2a179fb3e99e659e5f4ec0158
Luke Page <luke.a.page@gmail.com>
2014-10-18 23:14:04 +0100

    Upgrade to jasmine v2

--- test/browser/common.js
+++ test/browser/common.js
@@ -1,215 +1,162 @@
 /* record log messages for testing */
-// var logAllIds = function() {
-//   var allTags = document.head.getElementsByTagName('style');
-//   var ids = [];
-//   for (var tg = 0; tg < allTags.length; tg++) {
-//     var tag = allTags[tg];
-//     if (tag.id) {
-//       console.log(tag.id);
-//     }
-//   }
-// };
 
 var logMessages = [];
 window.less = window.less || {};
-less.loggers = [{
-    info: function(msg) {
-        logMessages.push(msg);
-    },
-    debug: function(msg) {
-        logMessages.push(msg);
-    },
-    warn: function(msg) {
-        logMessages.push(msg);
-    },
-    error: function(msg) {
-        logMessages.push(msg);
+less.loggers = [
+    {
+        info: function (msg) {
+            logMessages.push(msg);
+        },
+        debug: function (msg) {
+            logMessages.push(msg);
+        },
+        warn: function (msg) {
+            logMessages.push(msg);
+        },
+        error: function (msg) {
+            logMessages.push(msg);
+        }
     }
-}];
+];
 
-var testLessEqualsInDocument = function() {
-  testLessInDocument(testSheet);
+var testLessEqualsInDocument = function () {
+    testLessInDocument(testSheet);
 };
 
-var testLessErrorsInDocument = function(isConsole) {
+var testLessErrorsInDocument = function (isConsole) {
     testLessInDocument(isConsole ? testErrorSheetConsole : testErrorSheet);
 };
 
-var testLessInDocument = function(testFunc) {
-  var links = document.getElementsByTagName('link'),
-    typePattern = /^text\/(x-)?less$/;
+var testLessInDocument = function (testFunc) {
+    var links = document.getElementsByTagName('link'),
+        typePattern = /^text\/(x-)?less$/;
 
-  for (var i = 0; i < links.length; i++) {
-    if (links[i].rel === 'stylesheet/less' || (links[i].rel.match(/stylesheet/) &&
-      (links[i].type.match(typePattern)))) {
-      testFunc(links[i]);
+    for (var i = 0; i < links.length; i++) {
+        if (links[i].rel === 'stylesheet/less' || (links[i].rel.match(/stylesheet/) &&
+            (links[i].type.match(typePattern)))) {
+            testFunc(links[i]);
+        }
     }
-  }
 };
 
-var testSheet = function(sheet) {
-  it(sheet.id + " should match the expected output", function() {
-    var lessOutputId = sheet.id.replace("original-", ""),
-      expectedOutputId = "expected-" + lessOutputId,
-      lessOutputObj,
-      lessOutput,
-      expectedOutputHref = document.getElementById(expectedOutputId).href,
-      expectedOutput = loadFile(expectedOutputHref),
-      finished = false;
+var testSheet = function (sheet) {
+    it(sheet.id + " should match the expected output", function (done) {
+        var lessOutputId = sheet.id.replace("original-", ""),
+            expectedOutputId = "expected-" + lessOutputId,
+            lessOutputObj,
+            lessOutput,
+            expectedOutputHref = document.getElementById(expectedOutputId).href,
+            expectedOutput = loadFile(expectedOutputHref);
 
-    // Browser spec generates less on the fly, so we need to loose control
-    less.pageLoadFinished
-        .then(function() {
-            finished = true;
-        });
-    waitsFor(function() {
-      return finished;
-    }, "generation of " + lessOutputId + "", 700);
+        // Browser spec generates less on the fly, so we need to loose control
+        less.pageLoadFinished
+            .then(function () {
+                lessOutputObj = document.getElementById(lessOutputId);
+                lessOutput = lessOutputObj.innerText || lessOutputObj.innerHTML;
 
-    runs(function() {
-        lessOutputObj = document.getElementById(lessOutputId);
-        lessOutput = lessOutputObj.innerText || lessOutputObj.innerHTML;
+                expectedOutput
+                    .then(function (text) {
+                        expect(text).toEqual(lessOutput);
+                        done();
+                    });
+            });
     });
-
-    waitsFor(function() {
-      return expectedOutput.loaded;
-    }, "failed to load expected outout", 10000);
-
-    runs(function() {
-      // use sheet to do testing
-            expect(expectedOutput.text).toEqual(lessOutput);
-    });
-  });
 };
 
 //TODO: do it cleaner - the same way as in css
 
 function extractId(href) {
-  return href.replace(/^[a-z-]+:\/+?[^\/]+/i, '') // Remove protocol & domain
-  .replace(/^\//, '') // Remove root /
-  .replace(/\.[a-zA-Z]+$/, '') // Remove simple extension
-  .replace(/[^\.\w-]+/g, '-') // Replace illegal characters
-  .replace(/\./g, ':'); // Replace dots with colons(for valid id)
+    return href.replace(/^[a-z-]+:\/+?[^\/]+/i, '') // Remove protocol & domain
+        .replace(/^\//, '') // Remove root /
+        .replace(/\.[a-zA-Z]+$/, '') // Remove simple extension
+        .replace(/[^\.\w-]+/g, '-') // Replace illegal characters
+        .replace(/\./g, ':'); // Replace dots with colons(for valid id)
 }
 
-var testErrorSheet = function(sheet) {
-  it(sheet.id + " should match an error", function() {
-    var lessHref = sheet.href,
-      id = "less-error-message:" + extractId(lessHref),
-      //            id = sheet.id.replace(/^original-less:/, "less-error-message:"),
-      errorHref = lessHref.replace(/.less$/, ".txt"),
-      errorFile = loadFile(errorHref),
-      actualErrorElement,
-      actualErrorMsg;
-
-    // Less.js sets 10ms timer in order to add error message on top of page.
-    waitsFor(function() {
-      actualErrorElement = document.getElementById(id);
-      return actualErrorElement !== null;
-    }, "error message was not generated", 70);
-
-    runs(function() {
-      actualErrorMsg = actualErrorElement.innerText
-        .replace(/\n\d+/g, function(lineNo) {
-        return lineNo + " ";
-      })
-        .replace(/\n\s*in /g, " in ")
-        .replace("\n\n", "\n");
-    });
-
-    waitsFor(function() {
-      return errorFile.loaded;
-    }, "failed to load expected outout", 10000);
-
-    runs(function() {
-      var errorTxt = errorFile.text
-        .replace("{path}", "")
-        .replace("{pathrel}", "")
-        .replace("{pathhref}", "http://localhost:8081/test/less/errors/")
-        .replace("{404status}", " (404)");
-      expect(errorTxt).toEqual(actualErrorMsg);
-      if (errorTxt == actualErrorMsg) {
-        actualErrorElement.style.display = "none";
-      }
-        });
+var waitFor = function (waitFunc) {
+    return new Promise(function (resolve) {
+        var timeoutId = setInterval(function () {
+            if (waitFunc()) {
+                clearInterval(timeoutId);
+                resolve();
+            }
+        }, 5);
     });
 };
 
-var testErrorSheetConsole = function(sheet) {
-    it(sheet.id + " should match an error", function() {
-        var lessHref =  sheet.href,
+var testErrorSheet = function (sheet) {
+    it(sheet.id + " should match an error", function (done) {
+        var lessHref = sheet.href,
+            id = "less-error-message:" + extractId(lessHref),
+            errorHref = lessHref.replace(/.less$/, ".txt"),
+            errorFile = loadFile(errorHref),
+            actualErrorElement,
+            actualErrorMsg;
+
+        // Less.js sets 10ms timer in order to add error message on top of page.
+        waitFor(function () {
+            actualErrorElement = document.getElementById(id);
+            return actualErrorElement !== null;
+        }).then(function () {
+                actualErrorMsg = actualErrorElement.innerText
+                    .replace(/\n\d+/g, function (lineNo) {
+                        return lineNo + " ";
+                    })
+                    .replace(/\n\s*in /g, " in ")
+                    .replace("\n\n", "\n");
+                errorFile
+                    .then(function (errorTxt) {
+                        errorTxt = errorTxt
+                            .replace("{path}", "")
+                            .replace("{pathrel}", "")
+                            .replace("{pathhref}", "http://localhost:8081/test/less/errors/")
+                            .replace("{404status}", " (404)");
+                        expect(errorTxt).toEqual(actualErrorMsg);
+                        if (errorTxt == actualErrorMsg) {
+                            actualErrorElement.style.display = "none";
+                        }
+                        done();
+                    });
+            });
+    });
+};
+
+var testErrorSheetConsole = function (sheet) {
+    it(sheet.id + " should match an error", function (done) {
+        var lessHref = sheet.href,
             id = sheet.id.replace(/^original-less:/, "less-error-message:"),
             errorHref = lessHref.replace(/.less$/, ".txt"),
             errorFile = loadFile(errorHref),
             actualErrorElement = document.getElementById(id),
             actualErrorMsg = logMessages[logMessages.length - 1];
 
-        describe("the error", function() {
+        describe("the error", function () {
             expect(actualErrorElement).toBe(null);
-
         });
 
-        /*actualErrorMsg = actualErrorElement.innerText
-            .replace(/\n\d+/g, function(lineNo) { return lineNo + " "; })
-            .replace(/\n\s*in /g, " in ")
-            .replace("\n\n", "\n");*/
-
-        waitsFor(function() {
-            return errorFile.loaded;
-        }, "failed to load expected outout", 10000);
-
-        runs(function() {
-            var errorTxt = errorFile.text
-                .replace("{path}", "")
-                .replace("{pathrel}", "")
-                .replace("{pathhref}", "http://localhost:8081/browser/less/")
-                .replace("{404status}", " (404)")
-                .trim();
-            expect(errorTxt).toEqual(actualErrorMsg);
+        errorFile
+            .then(function (errorTxt) {
+                errorTxt
+                    .replace("{path}", "")
+                    .replace("{pathrel}", "")
+                    .replace("{pathhref}", "http://localhost:8081/browser/less/")
+                    .replace("{404status}", " (404)")
+                    .trim();
+                expect(errorTxt).toEqual(actualErrorMsg);
+                done();
+            });
     });
-  });
 };
 
-var loadFile = function(href) {
-  var request = new XMLHttpRequest(),
-    response = {
-      loaded: false,
-      text: ""
-    };
-  request.open('GET', href, true);
-  request.onload = function(e) {
-    response.text = request.response.replace(/\r/g, "");
-    response.loaded = true;
-  };
-  request.send();
-  return response;
+var loadFile = function (href) {
+    return new Promise(function (resolve, reject) {
+        var request = new XMLHttpRequest();
+        request.open('GET', href, true);
+        request.onload = function (e) {
+            resolve(request.response.replace(/\r/g, ""));
+        };
+        request.send();
+    });
 };
 
-(function() {
-  var jasmineEnv = jasmine.getEnv();
-  jasmineEnv.updateInterval = 1000;
-
-  var htmlReporter = new jasmine.HtmlReporter();
-
-  jasmineEnv.addReporter(htmlReporter);
-
-  jasmineEnv.specFilter = function(spec) {
-    return htmlReporter.specFilter(spec);
-  };
-
-  var currentWindowOnload = window.onload;
-
-  window.onload = function() {
-    if (currentWindowOnload) {
-      currentWindowOnload();
-    }
-    execJasmine();
-  };
-
-  function execJasmine() {
-    setTimeout(function() {
-      jasmineEnv.execute();
-    }, 3000);
-  }
-
-})();
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
--- test/browser/runner-modify-vars-spec.js
+++ test/browser/runner-modify-vars-spec.js
@@ -1,43 +1,33 @@
 var alreadyRun = false;
 
-describe("less.js modify vars", function() {
-  beforeEach(function() {
-    // simulating "setUp" or "beforeAll" method
-    var lessOutputObj;
-    if (alreadyRun)
-      return;
+describe("less.js modify vars", function () {
+    beforeEach(function (done) {
+        // simulating "setUp" or "beforeAll" method
+        if (alreadyRun) {
+            done();
+            return;
+        }
 
-    alreadyRun = true;
+        alreadyRun = true;
 
-    // wait until the sheet is compiled first time
-    waitsFor(function() {
-      lessOutputObj = document.getElementById("less:test-less-simple");
-      return lessOutputObj !== null;
-    }, "first generation of less:test-less-simple", 7000);
-
-    // modify variables
-    runs(function() {
-      lessOutputObj.type = "not compiled yet";
-      less.modifyVars({
-        var1: "green",
-        var2: "purple",
-        scale: 20
-      });
+        less.pageLoadFinished
+            .then(function () {
+                less.modifyVars({
+                    var1: "green",
+                    var2: "purple",
+                    scale: 20
+                }).then(function () {
+                        done();
+                    });
+            });
     });
 
-    // wait until variables are modified
-    waitsFor(function() {
-      lessOutputObj = document.getElementById("less:test-less-simple");
-      return lessOutputObj !== null && lessOutputObj.type === "text/css";
-    }, "second generation of less:test-less-simple", 7000);
-
-  });
-
-  testLessEqualsInDocument();
-  it("Should log only 2 XHR requests", function() {
-    var xhrLogMessages = logMessages.filter(function(item) {
-      return (/XHR: Getting '/).test(item);
+    testLessEqualsInDocument();
+    it("Should log only 2 XHR requests", function (done) {
+        var xhrLogMessages = logMessages.filter(function (item) {
+            return (/XHR: Getting '/).test(item);
+        });
+        expect(xhrLogMessages.length).toEqual(2);
+        done();
     });
-    expect(xhrLogMessages.length).toEqual(2);
-  });
 });

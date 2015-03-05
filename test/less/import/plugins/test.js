d119e018075647bb3911229835689a5d7656173b
rjgotten <r.otten@netmatch.nl>
2015-03-05 15:55:37 +0100

    Scoped @import (plugin) functions loading
    - Limited @import (plugin) support to add/addMultiple of functions
    - Altered @import (plugin) loading to support browser
    - Support proper closure scoping of @import (plugin) loaded functions

--- test/less/import/plugins/test.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    install : function( less, pluginManager ) {
-        less.functions.functionRegistry.add( "test", function() {
-            return new less.tree.Anonymous( "OK" );
-        });
-    }
-}

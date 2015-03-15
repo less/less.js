4bb098bfc94fc06da0cd1c9442e50e9181c77c0e
Bass Jobsen <bass@w3masters.nl>
2015-03-15 23:09:50 +0100

    fix for issue #2500

--- test/index.js
+++ test/index.js
@@ -47,6 +47,7 @@ lessTester.runTestSet({modifyVars: true}, "modifyVars/",
     null, null, null, function(name, type, baseFolder) { return path.join(baseFolder, name) + '.json'; });
 lessTester.runTestSet({urlArgs: '424242'}, "url-args/");
 lessTester.runTestSet({paths: ['test/data/', 'test/less/import/']}, "include-path/");
+lessTester.runTestSet({paths: 'test/data/'}, "include-path-string/");
 lessTester.testSyncronous({syncImport: true}, "import");
 lessTester.testSyncronous({syncImport: true}, "css");
 lessTester.testNoOptions();

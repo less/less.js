87f8b2d43e0964c17e1c494d5cba33b069922578
Matthew Dean <matthewdean.me@gmail.com>
2016-01-26 17:10:15 -0800

    Add test demonstrating arguments and semi-colon separators

--- test/less/plugin/plugin-root.js
+++ test/less/plugin/plugin-root.js
@@ -3,8 +3,8 @@ functions.addMultiple({
         var rule = new tree.Rule('prop', new tree.Anonymous('value'));
         return new tree.DetachedRuleset(new tree.Ruleset("", [ rule ]));
     },
-    "test-directive": function() {
-        return new tree.Directive("@charset", new tree.Anonymous('"utf-8"'));
+    "test-directive": function(arg1, arg2) {
+        return new tree.Directive(arg1.value, new tree.Anonymous('"' + arg2.value + '"'));
     },
     // Functions must return something. Must 'return true' if they produce no output.
     "test-undefined": function() { }

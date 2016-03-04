6e97db84b3c5668238b961215da806528c515ab2
Matthew Dean <matthewdean.me@gmail.com>
2016-03-04 14:10:53 -0700

    Tests passing for creating directives

--- test/less/plugin/plugin-root.js
+++ test/less/plugin/plugin-root.js
@@ -4,7 +4,7 @@ functions.addMultiple({
         return new tree.DetachedRuleset(new tree.Ruleset("", [ rule ]));
     },
     "test-directive": function(arg1, arg2) {
-        return new tree.Directive(arg1.value, new tree.Anonymous('"' + arg2.value + '"'));
+        return new tree.Directive(arg1.value, new tree.Anonymous(arg2.value));
     },
     // Functions must return something. Must 'return true' if they produce no output.
     "test-undefined": function() { }

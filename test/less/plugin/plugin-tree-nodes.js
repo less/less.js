39150d513a774ec5cff42a8cfe91eb4550dcd514
Matthew Dean <matthewdean.me@gmail.com>
2016-07-19 01:30:34 -0700

    Simplified API on less object e.g. "less.Ruleset()" instead of "new less.tree.Ruleset()"
    Auto-casting of string values into nodes for AtRule, Declaration, Selector, Value
    e.g. "less.Selector('&.a')" instead of "new tree.Selector(new tree.Element(new tree.Combinator('&'), '.a'))"

--- test/less/plugin/plugin-tree-nodes.js
+++ test/less/plugin/plugin-tree-nodes.js
@@ -1,10 +1,10 @@
 functions.addMultiple({
 
     "test-comment": function() {
-        return new tree.Combinator(' ');
+        return less.Combinator(' ');
     },
-    "test-directive": function(arg1, arg2) {
-        return new tree.Directive(arg1.value, new tree.Anonymous(arg2.value));
+    "test-atrule": function(arg1, arg2) {
+        return less.AtRule(arg1.value, arg2.value);
     },
     "test-extend": function() {
         //TODO
@@ -22,7 +22,7 @@ functions.addMultiple({
         //TODO
     },
     "test-ruleset-call": function() {
-        return new tree.Combinator(' ');
+        return less.Combinator(' ');
     },
     // Functions must return something, even if it's false/true
     "test-undefined": function() { 
@@ -33,52 +33,53 @@ functions.addMultiple({
     },
     // These cause root errors
     "test-alpha": function() {
-        return new tree.Alpha(30);
+        return less.Alpha(30);
     },
     "test-assignment": function() {
-        return new tree.Assignment("bird", "robin");
+        return less.Assignment("bird", "robin");
     },
     "test-attribute": function() {
-        return new tree.Attribute("foo", "=", "bar");
+        return less.Attribute("foo", "=", "bar");
     },
     "test-call": function() {
-        return new tree.Call("foo");
+        return less.Call("foo");
     },
     "test-color": function() {
-        return new tree.Color([50, 50, 50]);
+        return less.Color([50, 50, 50]);
     },
     "test-condition": function() {
-        return new tree.Condition('<', new tree.Value([0]), new tree.Value([1]));
+        return less.Condition('<', less.Value([0]), less.Value([1]));
     },
     "test-detached-ruleset" : function() {
-        var decl = new tree.Declaration('prop', new tree.Anonymous('value'));
-        return new tree.DetachedRuleset(new tree.Ruleset("", [ decl ]));
+        var decl = less.Declaration('prop', 'value');
+        return less.DetachedRuleset(less.Ruleset("", [ decl ]));
     },
     "test-dimension": function() {
-        return new tree.Dimension(1, 'px');
+        return less.Dimension(1, 'px');
     },
     "test-element": function() {
-        return new tree.Element('+', 'a');
+        return less.Element('+', 'a');
     },
     "test-expression": function() {
-        return new tree.Expression([1, 2, 3]);
+        return less.Expression([1, 2, 3]);
     },
     "test-keyword": function() {
-        return new tree.Keyword('foo');
+        return less.Keyword('foo');
     },
     "test-operation": function() {
-        return new tree.Operation('+', [1, 2]);
+        return less.Operation('+', [1, 2]);
     },
     "test-quoted": function() {
-        return new tree.Quoted('"', 'foo');
+        return less.Quoted('"', 'foo');
     },
     "test-selector": function() {
-        return new tree.Selector([new tree.Element('a')]);
+        var sel = less.Selector('.a.b');
+        return sel;
     },
     "test-url": function() {
-        return new tree.URL('http://google.com');
+        return less.URL('http://google.com');
     },
     "test-value": function() {
-        return new tree.Value([1]);
+        return less.Value([1]);
     }
 });

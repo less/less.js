983fa992b840cc05a84e0b53bc239aa8d60a4861
Matthew Dean <matthew-dean@users.noreply.github.com>
2018-06-24 13:01:30 -0700

    Fixes #3205, partial 3.0 math regression #1880

--- test/less/plugin/plugin-preeval.js
+++ test/less/plugin/plugin-preeval.js
@@ -1,6 +1,5 @@
 module.exports = {
     install({ tree: { Quoted }, visitors }, manager) {
-        global.TRACK = false
         class Visitor {
             constructor() {
                 this.native = new visitors.Visitor(this);
@@ -13,31 +12,8 @@ module.exports = {
                 return this.native.visit(root);
             }
 
-            visitDeclaration(node) {
-                if (node.name === '@stop') {
-                    global.TRACK = false;
-                }
-                console.log(node.name, node.value.type);
-                return node;
-            }
-
-            visitRuleset(node) {
-                if (global.TRACK) {
-                    // console.log(node.rules[0]);
-                }
-                return node;
-            }
-
-            visitMixinCall(node) {
-                console.log('mixin call');  // , node.arguments[0].value.ruleset.rules[0].value.value[0]
-                global.TRACK = true;
-                return node;
-            }
-
             visitVariable(node) {
-                // console.log(node.name);
-                if (node.name === '@foo') {
-                    console.log('visited var');
+                if (node.name === '@replace') {
                     return new Quoted(`'`, 'bar', true);
                 }
                 return node;

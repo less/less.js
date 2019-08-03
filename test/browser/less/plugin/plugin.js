13525ccbb601fc9406056a6138764b982cf1be3b
Matthew Dean <matthew-dean@users.noreply.github.com>
2019-08-03 10:27:47 -0700

    Simplify in-browser plugin

--- test/browser/less/plugin/plugin.js
+++ test/browser/less/plugin/plugin.js
@@ -1,7 +1,4 @@
-registerPlugin({
-    install: function(less, pluginManager, functions) {
-        functions.add('func', function() {
-            return less.anonymous(location.href);
-        });
-    }
+
+functions.add('func', function() {
+    return less.anonymous(location.href);
 });

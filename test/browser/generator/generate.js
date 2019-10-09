48ffb2e54f4b9c0c84a2de83622243a33b624fcf
Matthew Dean <matthew-dean@users.noreply.github.com>
2019-10-08 18:55:42 -0700

    Update tests to Mocha / Chai / Headless Chrome

---
+++ test/browser/generator/generate.js
@@ -0,0 +1,45 @@
+const template = require('./template')
+const config = require('./runner.config')
+const fs = require('fs-extra')
+const path = require('path')
+const globby = require('globby')
+const { runner } = require('mocha-headless-chrome')
+
+/**
+ * Generate templates and run tests
+ */
+const tests = []
+const cwd = process.cwd()
+const tmpDir = path.join(cwd, 'tmp', 'browser')
+fs.ensureDirSync(tmpDir)
+fs.copySync(path.join(cwd, 'test', 'browser', 'common.js'), path.join(tmpDir, 'common.js'))
+
+/** Will run the runners in a series */
+function runSerial(tasks) {
+  var result = Promise.resolve()
+  tasks.forEach(task => {
+    result = result.then(result => task(), err => {
+      console.error(err.message)
+    })
+  })
+  return result
+}
+
+Object.entries(config).forEach(entry => {
+  const test = entry[1]
+  const paths = globby.sync(test.src)
+  const templateString = template(paths, test.options.helpers, test.options.specs)
+  fs.writeFileSync(path.join(cwd, test.options.outfile), templateString)
+  tests.push(() => runner({
+    file: 'http://localhost:8081/' + test.options.outfile,
+    timeout: 2000,
+    args: ['disable-web-security']
+  }))
+})
+
+module.exports = () => runSerial(tests).then(() => {
+  process.exit()
+}, err => {
+  console.error(err.message)
+  process.exit()
+})
---
+++ test/browser/generator/template.js
@@ -0,0 +1,57 @@
+const html = require('html-template-tag')
+
+/**
+ * Generates HTML templates from list of test sheets
+ */
+module.exports = (stylesheets, helpers, spec) => {
+    if (!Array.isArray(helpers)) {
+        helpers = [helpers]
+    }
+    return html`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    
+    <title>Jasmine Spec Runner</title>
+
+    <!-- for each test, generate CSS/LESS link tags -->
+    $${stylesheets.map(function(fullLessName) {
+        var pathParts = fullLessName.split('/');
+        var fullCssName = fullLessName.replace(/less/g, 'css');
+        var lessName = pathParts[pathParts.length - 1];
+        var name = lessName.split('.')[0];
+        return `
+    <!-- the tags to be generated -->
+    <link id="original-less:test-less-${name}" title="test-less-${name}" rel="stylesheet/less" type="text/css" href="../../${fullLessName}">
+    <link id="expected-less:test-less-${name}" rel="stylesheet" type="text/css" href="../../${fullCssName}">
+    ` }).join('')}
+
+    $${helpers.map(helper => `
+        <script src="../../${helper}"></script>
+    `).join('')}
+
+    <link rel="stylesheet" href="../../node_modules/mocha/mocha.css">
+</head>
+
+<body>
+    <!-- content -->
+    <div id="mocha"></div>
+    <script src="../../node_modules/mocha/mocha.js"></script>
+    <script src="../../node_modules/mocha-teamcity-reporter/lib/teamcityBrowser.js"></script>
+    <script src="../../node_modules/chai/chai.js"></script>
+    <script>
+        expect = chai.expect
+        mocha.setup({
+            ui: 'bdd',
+            timeout: 1000
+        });
+    </script>
+    <script src="common.js"></script>
+    <script src="../../${spec}"></script>
+    <script src="less.min.js"></script>
+    <script>mocha.run();</script>
+</body>
+</html>
+`
+    }

a722237c1f6baba9bea82bce3e275b9e51311a00
Matthew Dean <matthew-dean@users.noreply.github.com>
2020-07-06 20:24:29 -0700

    Remove lib folder from git (#3531)
    * Remove lib from repo
    * Add lib folder to .gitignore
    * Remove src from publish

--- packages/less/lib/less/data/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var colors_1 = __importDefault(require("./colors"));
-var unit_conversions_1 = __importDefault(require("./unit-conversions"));
-exports.default = { colors: colors_1.default, unitConversions: unit_conversions_1.default };
-//# sourceMappingURL=index.js.map

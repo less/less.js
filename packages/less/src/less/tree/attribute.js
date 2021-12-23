2431015afa1c2454e27907cfefaface19ce1393d
Lei Chen <chenleicoder@foxmail.com>
2021-12-23 23:53:41 +0800

    feat: add support for case-insensitive attribute selectors (#3673)
    * feat: add support for case-insensitive attribute selectors

--- packages/less/src/less/tree/attribute.js
+++ packages/less/src/less/tree/attribute.js
@@ -1,17 +1,22 @@
 import Node from './node';
 
-const Attribute = function(key, op, value) {
+const Attribute = function(key, op, value, cif) {
     this.key = key;
     this.op = op;
     this.value = value;
+    this.cif = cif;
 }
 
 Attribute.prototype = Object.assign(new Node(), {
     type: 'Attribute',
 
     eval(context) {
-        return new Attribute(this.key.eval ? this.key.eval(context) : this.key,
-            this.op, (this.value && this.value.eval) ? this.value.eval(context) : this.value);
+        return new Attribute(
+            this.key.eval ? this.key.eval(context) : this.key,
+            this.op,
+            (this.value && this.value.eval) ? this.value.eval(context) : this.value,
+            this.cif
+        );
     },
 
     genCSS(context, output) {
@@ -24,6 +29,10 @@ Attribute.prototype = Object.assign(new Node(), {
         if (this.op) {
             value += this.op;
             value += (this.value.toCSS ? this.value.toCSS(context) : this.value);
+        }
+
+        if (this.cif) {
+            value = value + " " + this.cif;
         }
 
         return `[${value}]`;

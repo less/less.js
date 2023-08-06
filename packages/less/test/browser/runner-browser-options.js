876bf7c095665bd5ea01a7a180e1a6667989b903
iChenLei <chenleicoder@gmail.com>
2023-08-06 16:42:14 +0800

    chore: fix browser tests

--- packages/less/test/browser/runner-browser-options.js
+++ packages/less/test/browser/runner-browser-options.js
@@ -14,8 +14,8 @@ var testFiles = ['charsets', 'colors', 'comments', 'css-3', 'strings', 'media', 
 /**
  * @todo - generate the node_modules path for this file and in templates
  */
-var lessFolder = '../../../../node_modules/@less/test-data/less'
-var cssFolder = '../../../../node_modules/@less/test-data/css'
+var lessFolder = './node_modules/@less/test-data/less'
+var cssFolder = './node_modules/@less/test-data/css'
 
 for (var i = 0; i < testFiles.length; i++) {
     var file = testFiles[i],

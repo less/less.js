69f2010467f6eaf5e476f9c6d1af0a2362670d4b
Daniel Puckowski <puckowski.d@gmail.com>
2024-11-29 12:58:33 -0500

    fix(issue:4252) container query mixin reference
    * Fixes a container query mixin reference issue and adds container query
      test.

--- packages/less/src/less/tree/query-in-parens.js
+++ packages/less/src/less/tree/query-in-parens.js
@@ -1,3 +1,5 @@
+import { copy } from 'copy-anything';
+import Declaration from './declaration';
 import Node from './node';
 
 const QueryInParens = function (op, l, m, op2, r, i) {
@@ -7,6 +9,7 @@ const QueryInParens = function (op, l, m, op2, r, i) {
     this.op2 = op2 ? op2.trim() : null;
     this.rvalue = r;
     this._index = i;
+    this.mvalues = [];
 };
 
 QueryInParens.prototype = Object.assign(new Node(), {
@@ -22,7 +25,39 @@ QueryInParens.prototype = Object.assign(new Node(), {
 
     eval(context) {
         this.lvalue = this.lvalue.eval(context);
-        this.mvalue = this.mvalue.eval(context);
+        
+        let hasVariable = false;
+        let rule;
+
+        for (let i = 0; (rule = context.frames[i]); i++) {
+            if (rule.type === 'Ruleset') {
+                rule.rules.filter(function (r) {
+                    if ((r instanceof Declaration) && r.variable) {
+                        hasVariable = true;
+                    }
+                });
+                
+                if (hasVariable) {
+                    break;
+                }
+            }
+        }
+
+        if (!this.mvalueCopy) {
+            this.mvalueCopy = copy(this.mvalue);
+        }
+
+        if (hasVariable) {
+            this.mvalue = this.mvalueCopy;
+        }
+        
+        if (hasVariable) {
+            this.mvalue = this.mvalue.eval(context);
+            this.mvalues.push(this.mvalue);
+        } else {
+            this.mvalue = this.mvalue.eval(context);
+        }
+
         if (this.rvalue) {
             this.rvalue = this.rvalue.eval(context);
         }
@@ -32,6 +67,9 @@ QueryInParens.prototype = Object.assign(new Node(), {
     genCSS(context, output) {
         this.lvalue.genCSS(context, output);
         output.add(' ' + this.op + ' ');
+        if (this.mvalues.length > 0) {
+            this.mvalue = this.mvalues.shift();
+        }
         this.mvalue.genCSS(context, output);
         if (this.rvalue) {
             output.add(' ' + this.op2 + ' ');

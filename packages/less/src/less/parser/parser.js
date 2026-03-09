87bec519e73a105e7fdb742fd186910e6cebfb42
CommanderRoot <CommanderRoot@users.noreply.github.com>
2026-03-09 21:19:56 +0100

    chore: replace deprecated String.prototype.substr() (#3702)
    .substr() is deprecated so we replace it with .slice() which works similarily but isn't deprecated
    
    Signed-off-by: Tobias Speicher <rootcommander@gmail.com>

--- packages/less/src/less/parser/parser.js
+++ packages/less/src/less/parser/parser.js
@@ -406,7 +406,7 @@ const Parser = function Parser(context, imports, fileInfo, currentIndex) {
                     }
                     parserInput.forget();
 
-                    return new(tree.Quoted)(str.charAt(0), str.substr(1, str.length - 2), isEscaped, index + currentIndex, fileInfo);
+                    return new(tree.Quoted)(str.charAt(0), str.slice(1, -1), isEscaped, index + currentIndex, fileInfo);
                 },
 
                 //
@@ -788,7 +788,7 @@ const Parser = function Parser(context, imports, fileInfo, currentIndex) {
                     if (js) {
                         warn('Inline JavaScript evaluation (backtick expressions) is deprecated and will be removed in Less 5.x. Use Less functions or custom plugins instead.', index, 'DEPRECATED', 'js-eval');
                         parserInput.forget();
-                        return new(tree.JavaScript)(js.substr(0, js.length - 1), Boolean(escape), index + currentIndex, fileInfo);
+                        return new(tree.JavaScript)(js.slice(0, -1), Boolean(escape), index + currentIndex, fileInfo);
                     }
                     parserInput.restore('invalid javascript definition');
                 }
@@ -1901,7 +1901,7 @@ const Parser = function Parser(context, imports, fileInfo, currentIndex) {
                         }
                         if (parserInput.$char(')')) {
                             if (p && !e) {
-                                nodes.push(new (tree.Paren)(new (tree.QueryInParens)(p.op, p.lvalue, p.rvalue, rangeP ? rangeP.op : null, rangeP ? rangeP.rvalue : null, p._index)));				 
+                                nodes.push(new (tree.Paren)(new (tree.QueryInParens)(p.op, p.lvalue, p.rvalue, rangeP ? rangeP.op : null, rangeP ? rangeP.rvalue : null, p._index)));
                                 e = p;
                             } else if (p && e) {
                                 nodes.push(new (tree.Paren)(new (tree.Declaration)(p, e, null, null, parserInput.i + currentIndex, fileInfo, true)));
@@ -1986,12 +1986,12 @@ const Parser = function Parser(context, imports, fileInfo, currentIndex) {
                     if (parserInput.$str('@media')) {
                         return this.prepareAndGetNestableAtRule(tree.Media, index, debugInfo, MediaSyntaxOptions);
                     }
-                    
+
                     if (parserInput.$str('@container')) {
                         return this.prepareAndGetNestableAtRule(tree.Container, index, debugInfo, ContainerSyntaxOptions);
                     }
                 }
-                
+
                 parserInput.restore();
             },
 

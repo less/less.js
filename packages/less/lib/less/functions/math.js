cb4369afd5bb5fd0a14c728fa563b6b6685bb03b
林晨 (Leo Cheng) <leo-cheng@vip.qq.com>
2026-07-26 01:15:08 +0800

    fix: leave math functions for the browser when an argument is a runtime CSS var()
    Signed-off-by: 林晨 (Leo Cheng) <leo-cheng@vip.qq.com>

--- packages/less/lib/less/functions/math.js
+++ packages/less/lib/less/functions/math.js
@@ -1,29 +1,30 @@
 import mathHelper from './math-helper.js';
 
 const mathFunctions = {
-    // name,  unit
-    ceil:  null,
-    floor: null,
-    sqrt:  null,
-    abs:   null,
-    tan:   '',
-    sin:   '',
-    cos:   '',
-    atan:  'rad',
-    asin:  'rad',
-    acos:  'rad'
+    // name:  [unit, has a CSS equivalent that the browser can evaluate]
+    ceil:  [null, false],
+    floor: [null, false],
+    sqrt:  [null, true],
+    abs:   [null, true],
+    tan:   ['', true],
+    sin:   ['', true],
+    cos:   ['', true],
+    atan:  ['rad', true],
+    asin:  ['rad', true],
+    acos:  ['rad', true]
 };
 
 for (const f in mathFunctions) {
     // eslint-disable-next-line no-prototype-builtins
     if (mathFunctions.hasOwnProperty(f)) {
-        mathFunctions[f] = mathHelper.bind(null, Math[f], mathFunctions[f]);
+        const [unit, cssEvaluable] = mathFunctions[f];
+        mathFunctions[f] = mathHelper.bind(null, Math[f], unit, cssEvaluable);
     }
 }
 
 mathFunctions.round = (n, f) => {
     const fraction = typeof f === 'undefined' ? 0 : f.value;
-    return mathHelper(num => num.toFixed(fraction), null, n);
+    return mathHelper(num => num.toFixed(fraction), null, false, n);
 };
 
 export default mathFunctions;
